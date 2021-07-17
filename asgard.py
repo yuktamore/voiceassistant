ceb677820aebc943fe94e2158200315695671ac0
Yukta More <68540038+yuktamore@users.noreply.github.com>
2021-07-17 22:24:59 +0530

    Update asgard.py

--- asgard.py
+++ asgard.py
@@ -1,1 +1,10 @@
 #Author: YUKTA MORE
+from tkinter import *
+from tkinter.ttk import *
+import datetime
+import speech_recognition
+import wikipedia
+import pyjokes
+import os
+import random
+import subprocess
